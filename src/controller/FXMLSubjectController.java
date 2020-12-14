4816c39684862f2b0cbd395382817d2a99ccdccb
AttackShrimp <danielsantamarinapuertas@gmail.com>
2020-12-14 23:19:32 +0100

    Solved error when entering written name into Subject creation

--- src/controller/FXMLSubjectController.java
+++ src/controller/FXMLSubjectController.java
@@ -13,7 +13,9 @@ import java.net.URL;
 import java.time.Duration;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
@@ -61,12 +63,12 @@ public class FXMLSubjectController implements Initializable {
     
     ObservableList<Alumno> listaAlumnos = AccesoBD.getInstance().getTutorias().getAlumnosTutorizados();
     ObservableList<Asignatura> listaAsignaturas = AccesoBD.getInstance().getTutorias().getAsignaturas();
-    FilteredList<Alumno> filteredItems = new FilteredList<Alumno>(listaAlumnos, p -> true);
+    FilteredList<String> filteredItems;
     
     @FXML
     private Button addButton;
     @FXML
-    private ComboBox<Alumno> comboStudents;
+    private ComboBox<String> comboStudents;
     
     private Tutoria tutoria = new Tutoria();
     @FXML
@@ -76,16 +78,22 @@ public class FXMLSubjectController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        filteredItems = new FilteredList<String>(
+                FXCollections.observableArrayList(
+                        listaAlumnos.stream().map(Alumno::toString).collect(Collectors.toList())
+                ), p -> true);
+        
         // TODO
-        addButton.setDisable(true);
-        addButton.disableProperty().bind(comboStudents.getEditor().textProperty().isEmpty());
+        /*addButton.setDisable(true);
+        addButton.disableProperty().bind(comboStudents.getEditor().textProperty().isEmpty());*/
         
         datos = tutoria.getAlumnos();
         listLV.setItems(datos);
         listLV.setCellFactory(cel -> new SimpleAlumnoCell());
         
     
-        comboStudents.setCellFactory(cel -> new SimpleAlumnoCell()); 
+        //comboStudents.setCellFactory(cel -> new SimpleAlumnoCell()); 
         comboSubject.setCellFactory(cel -> new SimpleSubjectCell());
         comboSubject.setButtonCell(new SimpleSubjectCell());
         comboSubject.setItems(listaAsignaturas);
@@ -93,25 +101,16 @@ public class FXMLSubjectController implements Initializable {
 
         
         comboStudents.getEditor().textProperty().addListener((a,b,c) -> {
-            System.out.println(c);
-//            if(!comboStudents.getSelectionModel().isEmpty() && (c.length() - b.length() == 1)) {
-//                comboStudents.getSelectionModel().clearSelection();
-//                comboStudents.setValue(null);
-//            }
-
+            /*if(!comboStudents.getSelectionModel().isEmpty() && (c.length() - b.length() == 1)) {
+                comboStudents.getSelectionModel().clearSelection();
+                comboStudents.setValue(null);
+            }*/
+            comboStudents.setValue(c);
             if(!c.isEmpty()) {
                 comboStudents.show();
-                Alumno alumno = checkMemberOf(c);
-                if(alumno != null) {
-                    comboStudents.setValue(alumno);
-                }
             }
         
         });
-        
-        
-
-        
     }
     
     private void cancelar(ActionEvent event) {
@@ -120,43 +119,28 @@ public class FXMLSubjectController implements Initializable {
 
     @FXML
     private void addStudent(ActionEvent event) {
-        System.out.println(comboStudents.getEditor().getText());
-        if ( datos.contains(comboStudents.getValue()) ) {
-            errorLabel.setText("No se pueden añadir alumnos repetidos.");
-            comboStudents.getEditor().setText("");
-            comboStudents.getSelectionModel().clearSelection();
-            comboStudents.setValue(null);
-            comboStudents.requestFocus();
-        }
-        
-        else {
-            Alumno alumnoBuscado = checkMemberOf(comboStudents.getEditor().getText());
-            if (alumnoBuscado != null) {
-                datos.add(alumnoBuscado);
-                comboStudents.getEditor().setText("");
-                comboStudents.getSelectionModel().clearSelection();
-                comboStudents.setValue(null);
-                comboStudents.requestFocus();
-                listLV.refresh();
-            } else if ((!comboStudents.getEditor().getText().isEmpty())
-                    && (comboStudents.getEditor().getText().trim().length() != 0)) {
-                datos.add(comboStudents.getValue());
-                comboStudents.getEditor().setText("");
-                comboStudents.getSelectionModel().clearSelection();
-                comboStudents.setValue(null);
-                comboStudents.requestFocus();
+        Alumno alumno = checkMemberOf(comboStudents.getValue());
+        if(alumno != null) {
+            if (datos.contains(alumno) ) {
+                errorLabel.setText("No se pueden añadir alumnos repetidos.");
+            } else if (!comboStudents.getValue().equals("")) {
+                // Open alumno creation here
+                
+                datos.add(alumno);
                 listLV.refresh();
             }
+            comboStudents.getEditor().setText("");
+            comboStudents.getSelectionModel().clearSelection();
+            comboStudents.requestFocus();
         }
     }
 
     private Alumno checkMemberOf(String nombreAlumno){ //método que comprueba si texto del comboStudents es igual a algun alumno
         Alumno res = null;
-        for(int i = 0; i <= filteredItems.size()&& res == null; i++){
-            if((filteredItems.get(i).getNombre() + " " + filteredItems.get(i).getApellidos()).toUpperCase().equals(nombreAlumno.toUpperCase())) {
-                res = filteredItems.get(i);
+        for(int i = 0; i < listaAlumnos.size()&& res == null; i++){
+            if((listaAlumnos.get(i).getNombre() + " " + listaAlumnos.get(i).getApellidos()).toUpperCase().equals(nombreAlumno.toUpperCase())) {
+                res = listaAlumnos.get(i);
             }
-                
         }
         return res;
     }
@@ -186,7 +170,7 @@ public class FXMLSubjectController implements Initializable {
     
     //METODO COPIADO DE STACK OVERFLOW https://stackoverflow.com/questions/19010619/javafx-filtered-combobox https://stackoverflow.com/questions/46988860/java-fx-editable-combobox-with-objects
     private void formatoComboStudents() {
-        comboStudents.setConverter(new StringConverter<Alumno>() {
+        /*comboStudents.setConverter(new StringConverter<Alumno>() {
         @Override
         public String toString(Alumno alumno) {
             return alumno == null ? "" : alumno.getNombre() + " " + alumno.getApellidos();
@@ -196,17 +180,17 @@ public class FXMLSubjectController implements Initializable {
             public Alumno fromString(String string) {
                 throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
-        });
+        });*/
         
         
         comboStudents.getEditor().textProperty().addListener((obs, oldValue, newValue) -> { //no funciona del todo
             final TextField editor = comboStudents.getEditor();
-            final Alumno selected = comboStudents.getSelectionModel().getSelectedItem();
+            final String selected = comboStudents.getSelectionModel().getSelectedItem();
             
             Platform.runLater(() -> {
-                if (selected == null || !selected.equals(editor.getText())) {
+                if (selected == null || !selected.toString().equals(editor.getText())) {
                     filteredItems.setPredicate(item -> {
-                        if (((item.getNombre() + " " + item.getApellidos()).toUpperCase()).startsWith(newValue.toUpperCase())) {
+                        if ((item.toString().toUpperCase()).startsWith(newValue.toUpperCase())) {
                             return true;
                         } else {
                             return false;
